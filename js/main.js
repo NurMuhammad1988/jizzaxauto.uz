3685bf68a1bddffadaa16cc436dea0b85407bb01
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-09-02 04:46:52 +0500

    sss

--- js/main.js
+++ js/main.js
@@ -4,27 +4,24 @@
     // Spinner
     var spinner = function () {
         setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
+            if ($("#spinner").length > 0) {
+                $("#spinner").removeClass("show");
             }
         }, 1);
     };
     spinner(0);
-    
-    
+
     // Initiate the wowjs
     new WOW().init();
-
 
     // Sticky Navbar
     $(window).scroll(function () {
         if ($(this).scrollTop() > 200) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+            $(".sticky-top").addClass("shadow-sm").css("top", "0px");
         } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+            $(".sticky-top").removeClass("shadow-sm").css("top", "-100px");
         }
     });
-
 
     // Car Categories
     $(".categories-carousel").owlCarousel({
@@ -33,31 +30,30 @@
         dots: false,
         loop: true,
         margin: 25,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="fas fa-chevron-left"></i>',
-            '<i class="fas fa-chevron-right"></i>'
+            '<i class="fas fa-chevron-right"></i>',
         ],
         responsiveClass: true,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1,
             },
-            576:{
-                items:1
+            576: {
+                items: 1,
             },
-            768:{
-                items:1
+            768: {
+                items: 1,
             },
-            992:{
-                items:2
+            992: {
+                items: 2,
             },
-            1200:{
-                items:3
-            }
-        }
+            1200: {
+                items: 3,
+            },
+        },
     });
-
 
     // testimonial carousel
     $(".testimonial-carousel").owlCarousel({
@@ -67,52 +63,56 @@
         dots: true,
         loop: true,
         margin: 25,
-        nav : false,
-        navText : [
+        nav: false,
+        navText: [
             '<i class="fa fa-angle-right"></i>',
-            '<i class="fa fa-angle-left"></i>'
+            '<i class="fa fa-angle-left"></i>',
         ],
         responsiveClass: true,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1,
             },
-            576:{
-                items:1
+            576: {
+                items: 1,
             },
-            768:{
-                items:1
+            768: {
+                items: 1,
             },
-            992:{
-                items:2
+            992: {
+                items: 2,
             },
-            1200:{
-                items:2
-            }
-        }
+            1200: {
+                items: 2,
+            },
+        },
     });
-
 
     // Facts counter
     $('[data-toggle="counter-up"]').counterUp({
         delay: 5,
-        time: 2000
+        time: 2000,
     });
 
-
-   // Back to top button
-   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
-    } else {
-        $('.back-to-top').fadeOut('slow');
-    }
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $(".back-to-top").fadeIn("slow");
+        } else {
+            $(".back-to-top").fadeOut("slow");
+        }
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    $(".back-to-top").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
         return false;
     });
-
-
 })(jQuery);
+
+
+
+
+
+
+
+
 
